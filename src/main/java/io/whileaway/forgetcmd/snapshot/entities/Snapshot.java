2c495f76475b4523ebc33d5bed198abb494ac6f8
ThisSeanZhang <thisseanzhang@gmail.com>
2019-10-22 13:51:39 +0800

    ...

--- src/main/java/io/whileaway/forgetcmd/snapshot/entities/Snapshot.java
+++ src/main/java/io/whileaway/forgetcmd/snapshot/entities/Snapshot.java
@@ -2,10 +2,7 @@ package io.whileaway.forgetcmd.snapshot.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -14,6 +11,7 @@ public class Snapshot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long snapId;
+    @Column(columnDefinition="text")
     private String config;
     private String title;
     private Long createTime;
